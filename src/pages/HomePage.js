0de59043a181f447241b39248af15ba419d8fd42
momov9 <r0905334@student.thomasmore.be>
2023-11-11 15:08:56 +0100

    simple homePage to test my localhost

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Welcome to My Website</h1>
+            <p>This is the homepage of my website. You can add your content and components here.</p>
+            <button>Learn More</button>
+        </div>
+    );
+}
+
+export default HomePage;
