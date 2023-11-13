b396f963661e2a697046b65cf93b82b186f018a0
momov9 <r0905334@student.thomasmore.be>
2023-11-13 02:35:16 +0100

    added styled components to my filters and gave the page a nicer look, added a remove filter button

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,29 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+    AnimeCard,
+    AnimeContainer,
+    AnimeImage,
+    ClearFilterButton,
+    FilterInfo,
+    GenreButton,
+    GenreButtons,
+    Wrapper,
+    Footer, Content,
+} from "../components/StyledComponents";
+import { featuredAnime } from "../data";
 
-import styled from "styled-components";
+const HomePage = () => {
+    const [selectedGenre, setSelectedGenre] = useState(null);
 
-const Button = styled.button`
-  background-color: #007bff;
-  color: #fff;
-  border: none;
-  padding: 10px 20px;
-  cursor: pointer;
-`;
+    const handleGenreFilter = (genre) => {
+        setSelectedGenre(genre);
+        console.log(`Filtering by genre: ${genre}`);
+    };
 
-function HomePage() {
+    const clearGenreFilter = () => {
+        setSelectedGenre(null);
+        console.log("Filter cleared");
+    };
 
-
+    const filteredAnime = selectedGenre
+        ? featuredAnime.filter((anime) => anime.genre === selectedGenre)
+        : featuredAnime;
 
     return (
-        <div>
-            <h1>Welcome to My Website</h1>
-            <p>This is the homepage of my website. You can add your content and components here.</p>
+        <Wrapper>
+            <Content>
+
+            <div>
+                <p>
+                    <strong>Welcome to the world of anime!</strong>
+                </p>
+                <p>
+                    If you're an anime enthusiast, you've come to the right place. Explore a wide
+                    selection of your favorite anime series and discover new adventures.
+                </p>
+                <p>
+                    Whether you're into action, adventure, or any other genre, our collection has
+                    something for everyone. Start your anime journey here!
+                </p>
+            </div>
 
 
 
-            <Button onClick={() => alert('You can learn more about my website here!')}>Learn More</Button>
-        </div>
+            <div>
+                <h2>Featured Anime</h2>
+                <AnimeContainer>
+                    {filteredAnime.map((anime) => (
+                        <AnimeCard key={anime.id}>
+                            <AnimeImage src={anime.image} alt={anime.title} />
+                            <p>{anime.title}</p>
+                        </AnimeCard>
+                    ))}
+                </AnimeContainer>
+            </div>
+
+                <GenreButtons>
+                <GenreButton onClick={() => handleGenreFilter("action")}>Action</GenreButton>
+                <GenreButton onClick={() => handleGenreFilter("adventure")}>Adventure</GenreButton>
+            </GenreButtons>
+
+                {selectedGenre && (
+                    <FilterInfo>
+                        <p>Filtering by: {selectedGenre}</p>
+                        <ClearFilterButton onClick={clearGenreFilter}>Clear Filter</ClearFilterButton>
+                    </FilterInfo>
+                )}
+            </Content>
+            <Footer>
+                <p>&copy;Anime World</p>
+                <p>Contact: info@animeworld.com</p>
+                <p>Follow us on social media: @AnimeWorld</p>
+            </Footer>
+        </Wrapper>
     );
-}
+};
 
 export default HomePage;
