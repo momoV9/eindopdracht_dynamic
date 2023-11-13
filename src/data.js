c5e661be062abc89941b08905f0289f8d01f7828
momov9 <r0905334@student.thomasmore.be>
2023-11-13 02:31:48 +0100

    moved my anime titles to a data.js file

---
+++ src/data.js
@@ -0,0 +1,7 @@
+export const featuredAnime = [
+    { id: 1, title: "Naruto", image: "https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/e6b2cd29a5ff62f4591d3b299007e24e.jpe", genre: "action" },
+    { id: 2, title: "One Piece", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQX_-zAqLigQEpKzAQY4OaS5YHyWLdyXoXdhw&usqp=CAU", genre: "adventure" },
+    { id: 3, title: "Bleach", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQtKGt0LUW54jK4rxO5q_tLqpVPARENhw09Q&usqp=CAU", genre: "action" },
+    { id: 4, title: "Solo leveling", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6NdXune2kp09mIi-gbhreAkQyarpyez76Tw&usqp=CAU", genre: "adventure" },
+    { id: 5, title: "Demon Slayer", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSByX797-Q8xeIcVxWNq65gk8tpT6J5whT7jg&usqp=CAU", genre: "action" },
+];
